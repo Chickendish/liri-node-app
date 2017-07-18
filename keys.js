76ecd40b9dcf2fe448767e18ccbb397a9bfb959f
Bill Donovan <williamcdonovan@gmail.com>
2017-07-18 14:06:42 -0600

    added Twitter keys

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'quEksrI02U4XuElDeuAttQVCr',
+  consumer_secret: 'xipbJX0PwbeRnLw81X2oGVz92bKIBV1DI3fWqPOr4PR0b3jaLH',
+  access_token_key: '887386392556425216-iqmKv9UHewLdxIqVLWX6WPvPG1XkOU6',
+  access_token_secret: '21tdNBh0oHNOLRgDk6ZNwRL8qdmrTkn9cg97YV7wutJT0',
